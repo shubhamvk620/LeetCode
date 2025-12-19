e3460bb41f229e83e09cd5eda24aa55eafc4dfb9
Shubham Kumar Verma <shubhamvk725@gmail.com>
2025-12-20 04:26:28 +0530

    Time: 7 ms (61.34%), Space: 47 MB (7.82%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -3,15 +3,13 @@ class Solution {
         Arrays.sort(nums);
         int count = 0;
         int i =0, j=1;
-         while(i < j){
+         while(j < nums.length){
             if(nums[i] == nums[j]){
                 count++;
                 i+=2; j+=2;
-                if( count == nums.length/2)
-                return true;
-            }
-            else return false;
+            }else return false;
+              
            }
-           return false;
+            return count == nums.length/2;
     }
 }
