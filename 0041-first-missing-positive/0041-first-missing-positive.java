b87dd25076ea4d6a73223f5e30b615b29080fb8d
Shubham Kumar Verma <shubhamvk725@gmail.com>
2025-12-29 05:04:18 +0530

    Time: 1 ms (99.8%), Space: 70.9 MB (88.43%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,17 +2,15 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int i = 0;
         while(i < nums.length){
-
-            
-            if(nums[i] == i+1 || nums[i] <= 0 || nums[i] > nums.length){i++;}
-            else{
-                int temp = nums[i]; // temp mein wo value rakhi jise swap karna hai
-                nums[i] = nums[temp-1]; //jise swap krna hai uski sahi index pe jo value baitha use nums[i] me put kro
-                nums[temp-1] = temp; // temp ko uski sahi seat par baitha diya
-
-                //agar sawp krne k baar value swap value or placed value sdame hai toh i++
-            if(nums[i] == nums[temp-1])i++;
-            }
+            int correctIndex = nums[i]-1;
+           if(nums[i] >=1 && nums[i] <= nums.length && nums[i] != nums[correctIndex]){
+            int temp = nums[i];
+            nums[i] = nums[correctIndex];
+            nums[correctIndex] = temp;
+           }
+           else{
+            i++;
+           }
         }
         for(int j = 0; j < nums.length; j++){
             if(nums[j]!= j+1)
