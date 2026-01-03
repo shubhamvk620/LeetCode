b9ba0feef8be342df5caddd25c59386b17f52607
Shubham Kumar Verma <shubhamvk725@gmail.com>
2026-01-04 00:47:04 +0530

    Time: 10 ms (85.89%), Space: 50.6 MB (6.55%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,4 +1,18 @@
 class Solution {
+    public static boolean isvalid(int[] weights, int days, int minday){
+        int day = 1;               
+            int minLoad = 0;
+
+            // calculate minload of ship and day taken for laoding.    
+            for(int k = 0; k < weights.length; k++){
+                if(minLoad + weights[k] <= minday){
+                minLoad += weights[k];
+                }else {
+                day++;
+             minLoad = weights[k];}
+            }
+            return day <= days;
+    }
     public int shipWithinDays(int[] weights, int days) {
 
        if(days > weights.length) return -1;
@@ -19,20 +33,10 @@ class Solution {
         int ans = -1;
 
         while(min <= max){
-            int mid = min+(max-min)/2; //mid, day and load all three value change and start from 
-            int day = 1;               //new value for every next day so written inside loop 
-            int minLoad = 0;
-
-            // calculate minload of ship and day taken for laoding.    
-            for(int k = 0; k < weights.length; k++){
-                if(minLoad + weights[k] <= mid){
-                minLoad += weights[k];
-                }else {
-                day++;
-             minLoad = weights[k];}
-            }
+            int mid = min+(max-min)/2; //mid(maxWieight), day and load all three value change and start from 
+            
             // if possible then put mid in ans.
-             if(day <= days){
+             if(isvalid(weights, days, mid)){
                   ans = mid;
                 max = mid-1;
               
