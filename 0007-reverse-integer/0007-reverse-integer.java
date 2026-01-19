4683e8123c3b96e1710a2cc7800c06f734f922e4
Shubham Kumar Verma <shubhamvk725@gmail.com>
2026-01-20 01:20:09 +0530

    Time: 1 ms (99.74%), Space: 42.4 MB (92.84%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,15 +1,16 @@
 class Solution {
     public int reverse(int x) {
         int dup = x;
-        long rev = 0;
+        long rev = 0;// Use long to capture values outside the int range
         while(dup!=0){
             rev = (dup%10)+(rev*10);
             dup/=10;
+            // Check if the current long value exceeds int bounds
           if (rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) {
     return 0;
 }
 
         }
-        return (int)rev;
+        return (int)rev; // Cast back to int before returning
     }
 }
