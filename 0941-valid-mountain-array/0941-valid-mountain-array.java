162c6378e2d3fd9203556adfcfbbf4441e1602b1
Shubham Kumar Verma <shubhamvk725@gmail.com>
2026-01-24 00:59:40 +0530

    Time: 2 ms (85.51%), Space: 47.6 MB (39.31%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -6,20 +6,14 @@ class Solution {
         int i = 0;
         int j = n - 1;
 
-        // Move 'i' forward as long as the mountain is strictly increasing
-        while (i + 1 < n && arr[i] < arr[i + 1]) {
+        while (i < n-1 && arr[i] < arr[i + 1]) {
             i++;
         }
 
-        // Move 'j' backward as long as the mountain is strictly decreasing
         while (j > 0 && arr[j] < arr[j - 1]) {
             j--;
         }
 
-        // The logic for a valid mountain:
-        // 1. They must meet at the same peak (i == j)
-        // 2. The peak cannot be the first element (i > 0)
-        // 3. The peak cannot be the last element (j < n - 1)
         return i > 0 && i == j && j < n - 1;
     }
 }
