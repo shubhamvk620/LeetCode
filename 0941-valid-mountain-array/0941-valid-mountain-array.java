22a3daaa7214696b2055126749df423e6f102d8f
Shubham Kumar Verma <shubhamvk725@gmail.com>
2026-01-24 01:02:34 +0530

    Time: 1 ms (99.67%), Space: 47.5 MB (39.31%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -5,11 +5,11 @@ class Solution {
 
         int i = 0;
         int j = n - 1;
-
+        //increase i jab tak wo last index k 1 pahle na pahunch jaye && arr[i+1] > arr[i] always
         while (i < n-1 && arr[i] < arr[i + 1]) {
             i++;
         }
-
+        //decrease j jabtak j 1st endex pe na pahunch jaye and arr[j] > arr[j-1] always.
         while (j > 0 && arr[j] < arr[j - 1]) {
             j--;
         }
