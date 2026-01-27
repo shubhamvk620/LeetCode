6975ebdf2d28e53b9e7c38bd68c4e580ec79425b
Shubham Kumar Verma <shubhamvk725@gmail.com>
2026-01-27 05:55:14 +0530

    Time: 1 ms (37.02%), Space: 46.4 MB (45.02%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -18,20 +18,21 @@ class Solution {
         while(count <= k && curr!=null){
             ListNode next = curr.next;
             curr.next = prev;
-            prev = curr;
-            curr = next;
+            prev = curr; // all reversing is saving in prev
+            curr = next; // reversing node until k count.
             count ++;
-           if(count == k){
-            temp.next = prev;
-            prev = null;
-            count = 0;
+           if(count == k){ // is count == k, means k tak reverse ho chuka hai & prev me rakh hai wo reverse nodes.
+            temp.next = prev; // prev ko temp.next me daal dena hai
+            prev = null; // make prev null again;
+            count = 0; // make count 0 again.
            }
-           if(count == 0){
-            while(temp.next != null){
+           if(count == 0){// count = 0 means reverse done & prev append in next of temp
+            while(temp.next != null){ // move temp to last node again means before null so to append next reverse nodes.
                 temp = temp.next;
             }
            }
-        }
+        } 
+        // loop for reversing those nodes that are less that k and got reversed and stayed in prev.
          ListNode prev1 = null;
         ListNode curr2 = prev;
         while(curr2!=null){
@@ -40,6 +41,7 @@ class Solution {
             prev1 = curr2;
             curr2 = next2;
         }
+        // after reversing left reversed nodes,& add them next to temp.
         temp.next = prev1;
         return dummy.next;
     }
