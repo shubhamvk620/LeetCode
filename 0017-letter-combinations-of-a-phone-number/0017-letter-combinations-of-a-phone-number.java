c2a7d8a55fdc83448d07f26dc8e1062ec23c498a
Shubham Kumar Verma <shubhamvk725@gmail.com>
2026-02-08 01:37:45 +0530

    Time: 3 ms (33.49%), Space: 43.8 MB (39.77%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,29 +1,37 @@
+
 class Solution {
-    public static void letterCombinations(String digits,List<String> ans,  HashMap<Integer,String> map, String curr,int i){
-        if( i == digits.length()){
+    public static void letterCombinations(String digits, List<String> ans,  HashMap<Integer,String> map, String curr, int i){
+        //base case
+        if(i == digits.length()){
             ans.add(curr);
             return;
         }
-        String temp = map.get(digits.charAt(i)-'0');
+        String temp = map.get(digits.charAt(i)-'0'); // sub with ASCCAI value of 0 becz digits.charAt(i) bring Integer's ASCAII value becz digits are pass in String
+
+        //loop for adding temp element to curr element
         for(int j = 0; j < temp.length(); j++){
+            //recursion call for i+1 and add temp[j] to curr
             letterCombinations(digits, ans, map, curr+temp.charAt(j), i+1);
-        }
+            //no need for backtracking changes since String is immutabale can't effect previous call element
+        }  
     }
     public List<String> letterCombinations(String digits) {
-        HashMap<Integer,String> map = new HashMap<>();
-        map.put(0,"");
-        map.put(1,"");
-        map.put(2,"abc");
-        map.put(3,"def");
-        map.put(4,"ghi");
-        map.put(5,"jkl");
-        map.put(6,"mno");
-        map.put(7,"pqrs");
-        map.put(8,"tuv");
-        map.put(9,"wxyz");
-        List<String> ans =new ArrayList<>();
-        String curr = "";
-        letterCombinations(digits, ans, map, curr, 0);
-        return ans;
+     HashMap<Integer,String> map = new HashMap<>();
+     map.put(0,"");
+     map.put(1,"");
+     map.put(2,"abc");
+     map.put(3,"def");
+     map.put(4,"ghi");
+     map.put(5,"jkl");
+     map.put(6,"mno");
+     map.put(7,"pqrs"); 
+     map.put(8,"tuv");
+     map.put(9,"wxyz");
+
+     List<String> ans = new ArrayList<>();
+     String curr = "";
+     letterCombinations(digits, ans, map, curr, 0);
+     return ans;
+       
     }
 }
