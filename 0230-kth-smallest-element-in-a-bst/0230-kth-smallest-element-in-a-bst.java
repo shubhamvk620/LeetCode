8d52378c602a1b30fa2275c5dd9dab51ed1a0340
Shubham Kumar Verma <shubhamvk725@gmail.com>
2026-03-03 00:29:53 +0530

    Time: 0 ms (100%), Space: 46.7 MB (81.58%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -13,23 +13,52 @@
  *     }
  * }
  */
-class Solution {
-    public static void smallest(TreeNode root, int k, List<Integer>list){
-        if(root == null) return;
+class Solution{
+    int ans = 0;
+    int c = 0;
+     public void smallest(TreeNode root, int k){
+         if(root == null) return;
+        
+        //first left call until last;
+         smallest(root.left, k);
 
-        smallest(root.left, k, list);
-        list.add(root.val);
-        smallest(root.right, k, list);
-    }
-    public int kthSmallest(TreeNode root, int k) {
-        if(root == null) return 0;
-        List<Integer>list = new ArrayList<>();
-        smallest(root, k, list);
-
-        int[] arr = new int[list.size()];
-         for(int i = 0; i < list.size(); i++){
-            arr[i] = list.get(i);
+         //backtrack hoga then count++
+         c++;
+         if(c == k){
+         ans = root.val;
+         return;
          }
-            return arr[k-1];
-    }
+
+         //right call;
+         smallest(root.right, k);
+         
+
+     }
+     public int kthSmallest(TreeNode root, int k){
+        if(root == null) return 0;
+         smallest(root, k);
+         return ans;
+     }
 }
+
+// class Solution {
+//     public static void smallest(TreeNode root, int k, List<Integer>list){
+//         if(root == null) return;
+
+//         smallest(root.left, k, list);
+//         list.add(root.val);
+//         smallest(root.right, k, list);
+
+//     }
+//     public int kthSmallest(TreeNode root, int k) {
+//         if(root == null) return 0;
+//         List<Integer>list = new ArrayList<>();
+//         smallest(root, k, list);
+
+//         int[] arr = new int[list.size()];
+//          for(int i = 0; i < list.size(); i++){
+//             arr[i] = list.get(i);
+//          }
+//             return arr[k-1];
+//     }
+// }
