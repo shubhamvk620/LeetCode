4ca9bcd4116f25750ae9fcc951a2c8650f5545cc
Shubham Kumar Verma <shubhamvk725@gmail.com>
2026-03-16 03:00:48 +0530

    Time: 3 ms (21.08%), Space: 43.6 MB (95.74%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+
+        if(k == 0){
+            for(int i = 0; i < n; i++){
+                code[i] = 0;
+            }
+            return code;
+        }
+
+        int[] arr = new int[n];
+
+        if(k > 0){
+        for(int q = 0; q < arr.length; q++){
+            int count = 0;
+            int add = 0;
+            int v = q;
+            while(count < k){
+                add += code[(v + 1)%n];
+                v++;
+                count++;
+            }
+            arr[q] = add;
+        }
+       }
+
+         if(k < 0){
+        for(int q = 0; q < arr.length; q++){
+            int count = 0;
+            int add = 0;
+            int v = q;
+            while(count < Math.abs(k)){
+                add += code[(v - 1 +n)%n];
+                v--;
+                count++;
+            }
+            arr[q] = add;
+        }
+       }
+
+       return arr;
+    }
+}
